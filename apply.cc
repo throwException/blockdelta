3ebac21372608097deaf2b9f8dc5c29e47484735
Stefan Thöni <stefan@savvy.ch>
2023-02-22 18:32:45 +0100

    apply: better error handling

--- apply.cc
+++ apply.cc
@@ -24,39 +24,60 @@ int Apply::run(char* blockpath)
 		return -2;
 	}
 
-	bool success = true;
+	int return_value = 0;
 	uint64_t block_number;
 	uint8_t buffer[BLOCK_SIZE];
 
 	while (true) {
 		if (fread(&block_number, sizeof(uint64_t), 1, stdin) != 1) {
-			break;
+			if (feof(stdin)) {
+				break;
+			} else {
+				fprintf(stderr, "Error reading stdin\n");
+				return_value = -5;
+				break;
+			}
 		}
 		if (fread(buffer, BLOCK_SIZE, 1, stdin) != 1) {
-			break;
+			if (feof(stdin)) {
+				fprintf(stderr, "Unexpected EOF at stdin\n");
+				return_value = -6;
+				break;
+			} else {
+				fprintf(stderr, "Error reading stdin\n");
+				return_value = -7;
+				break;
+			}
 		}
 		block_number = host_to_big_endian(block_number); 
 		if (fseek(block, block_number * BLOCK_SIZE, SEEK_SET) != 0) {
-			success = false;
+			fprintf(stderr, "Error seeking position in block file\n");
+			return_value = -7;
 			break;
 		}
 		if (fwrite(buffer, BLOCK_SIZE, 1, block) != 1) {
-			success = false;
+			fprintf(stderr, "Error writing to block file\n");
+			return_value = -8;
 			break;
 		}
 	}
 
-	if (ferror(stdin) != 0) {
-		success = false;
+	if ((return_value == 0) &&
+	    (ferror(stdin) != 0))
+	{
+		fprintf(stderr, "I/O error at stdin\n");
+		return_value = -9;
+	}
+
+	if ((return_value == 0) &&
+	    (ferror(block) != 0))
+	{
+		fprintf(stderr, "I/O error at block file\n");
+		return_value = -10;
 	}
 
 	fclose(block);
 
-	if (success) {
-		return 0;
-	} else {
-		fprintf(stderr, "I/O error\n");
-		return -5;
-	}
+	return return_value;
 }
 
